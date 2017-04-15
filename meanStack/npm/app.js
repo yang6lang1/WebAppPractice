d61a2c2129644e200d73481cfa02e4b8feb08d91
Alex Yang <guang.alex.yang@gmail.com>
2017-04-14 20:49:50 -0700

    Learned express router (USEFUL!!!)

--- meanStack/npm/app.js
+++ meanStack/npm/app.js
@@ -2,28 +2,18 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-
 app.set('port', 3000);
 
+// loggin middleware
+app.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.get('/homepage', function(req, res) {
-    console.log('GET the home page');
-    res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/json', function(req, res) {
-    console.log('GET the json');
-    res.status(200).json({
-        jsonData: true
-    });
-});
-
-app.get('/file', function(req, res) {
-    console.log('GET the file');
-    res.status(200).sendFile(path.join(__dirname, "app.js"));
-});
+var routes = require('./routes');
+app.use('/api', routes);
 
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
---
+++ meanStack/npm/app_2.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.set('port', 3000);
+
+// loggin middleware
+app.use('/css', function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/homepage', function(req, res) {
+    console.log('GET the home page');
+    res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/json', function(req, res) {
+    console.log('GET the json');
+    res.status(200).json({
+        jsonData: true
+    });
+});
+
+app.get('/file', function(req, res) {
+    console.log('GET the file');
+    res.status(200).sendFile(path.join(__dirname, "app.js"));
+});
+
+var server = app.listen(app.get('port'), function() {
+    var port = server.address().port;
+    console.log('magic happens on port ' + port);
+});
