3eb41fd2a363e9fd2dd1b703edbf6c81a7161c4e
Alex Yang <guang.alex.yang@gmail.com>
2019-01-29 22:14:37 -0800

    Finished webpack-react tutorial. Moving towards react + redux.

--- webpack-react-tutorial/src/js/components/container/FormContainer.jsx
+++ webpack-react-tutorial/src/js/components/container/FormContainer.jsx
@@ -34,3 +34,6 @@ class FormContainer extends Component {
   }
 }
 export default FormContainer;
+
+const wrapper = document.getElementById("create-article-form");
+wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
