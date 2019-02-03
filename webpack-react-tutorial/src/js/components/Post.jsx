2a060a7e66edf4b57ad2a7aed7e5ebeb03a2ca36
Alex Yang <guang.alex.yang@gmail.com>
2019-02-02 21:37:07 -0800

    Finishes the tutorial for redux-thunk A few options to achieve async logic in redux: - custom middleWare - redux-thunk: which allows an action to return a function instead of only object - redux-sage: for more complicated   async logic

--- webpack-react-tutorial/src/js/components/Post.jsx
+++ webpack-react-tutorial/src/js/components/Post.jsx
@@ -10,12 +10,27 @@ export class Post extends Component {
       // calling the new action creator
       this.props.getData();
     }
+
     render() {
-      return null;
+      return (
+        <ul className="list-group list-group-flush">
+          {this.props.articles.map(el => (
+            <li className="list-group-item" key={el.id}>
+              {el.title}
+            </li>
+          ))}
+        </ul>
+      );
     }
 }
 
+function mapStateToProps(state) {
+  return {
+    articles: state.remoteArticles.slice(0, 10)
+  };
+}
+
 export default connect(
-    null,
+    mapStateToProps,
     { getData }
 )(Post);
